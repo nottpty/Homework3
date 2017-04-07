87caf2ffd21ab4260d6be58da8e9fe9950423917
Patinya Yongyai <patinya.y@ku.th>
2017-04-07 19:46:49 +0700

    Add source file, gitignore and readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rucursion
+
+Method in this class can remove duplicate consecutive items from a list, changing the list parameter. For example, if list = { a, b, b, b, c, b, c, c} then after calling unique the list will contain {a, b, c, b, c}. Only consecutive duplicates are removed.
---
+++ src/Recursion.java
@@ -0,0 +1,33 @@
+import java.util.List;
+
+/**
+ * This class contain recursion method.
+ * 
+ * @author Patinya Yongyai
+ *
+ */
+public class Recursion {
+	/**
+	* Remove duplicate consecutive items from a list, changing the list parameter.
+	* For example, if list = { a, b, b, b, c, b, c, c} then after calling unique the list
+	* will contain {a, b, c, b, c}. Only consecutive duplicates are removed.
+	* Objects are compared using their own equals method.
+	* @param list of any kind of object. The elements are not null.
+	* @return reference to the list parameter with consecutive duplicates removed. */
+	public static List unique(List list){
+		try {
+			if(list.size() == 1){
+				return list;
+			}
+			if(list.get(list.size()-1).equals(list.get(list.size()-2))){
+				list.subList(0, list.size());
+				list.remove(list.size()-1);
+				return unique(list);
+			}
+			unique(list.subList(0, list.size()-1));
+			return list;
+		} catch(Exception e) {
+			throw new NullPointerException();
+		}
+	}
+}
